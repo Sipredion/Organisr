6b8b8df2b142c695968fb05bef5e16f12efa8d72
Michael <mike@solinta.co.za>
2019-07-18 16:23:00 +0200

    Fix breaking bug

--- libs/ui/src/lib/modules/modal/services/modal.service.ts
+++ libs/ui/src/lib/modules/modal/services/modal.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, TemplateRef } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
-import { ModalModule } from '@organisr/ui';
 
 @Injectable({
   providedIn: 'root'
